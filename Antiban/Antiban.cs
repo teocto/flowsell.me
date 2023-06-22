24fdfa29a0943872df67c828e95dcb5f838cb35d
Yeskendir <eskendir.toleubekov@gmail.com>
2023-06-22 15:32:16 +0600

    проходит тесты

--- Antiban/Antiban.cs
+++ Antiban/Antiban.cs
@@ -1,16 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Antiban
 {
     public class Antiban
     {
+        List<AntibanResult> results = new List<AntibanResult>();
+
         /// <summary>
         /// Добавление сообщений в систему, для обработки порядка сообщений
         /// </summary>
         /// <param name="eventMessage"></param>
         public void PushEventMessage(EventMessage eventMessage)
         {
-            //TODO
+            var ret = new AntibanResult() { 
+                EventMessageId = eventMessage.Id, 
+                SentDateTime = eventMessage.DateTime, 
+                Phone = eventMessage.Phone,
+                Priority = eventMessage.Priority
+            };
+            if (results.Any(x => x.Phone == eventMessage.Phone))
+            {
+                var last = results.Where(x => x.Phone == eventMessage.Phone && x.Priority == eventMessage.Priority && eventMessage.Priority == 1).OrderByDescending(x => x.SentDateTime).FirstOrDefault();
+                if (last != null)
+                {
+                    if ((eventMessage.DateTime - last.SentDateTime).TotalHours < 24)
+                    {
+                        ret.SentDateTime = last.SentDateTime.AddDays(1);
+                    }
+                }
+                else
+                {
+                    last = results.Where(x => x.Phone == eventMessage.Phone && x.SentDateTime <= ret.SentDateTime).OrderByDescending(x => x.SentDateTime).FirstOrDefault();
+                    if ((ret.SentDateTime - last.SentDateTime).TotalMinutes < 1)
+                        ret.SentDateTime = last.SentDateTime.AddMinutes(1);
+                }
+            }
+            if (results.Count > 0)
+            {
+                var last = results.Where(x => x.SentDateTime<ret.SentDateTime.AddSeconds(10)).OrderByDescending(x => x.SentDateTime).FirstOrDefault();
+                if((ret.SentDateTime-last.SentDateTime).TotalSeconds < 10)
+                {
+                    ret.SentDateTime = last.SentDateTime.AddSeconds(10);
+                }
+            }
+
+            results.Add(ret);
+            results = results.OrderBy(x => x.SentDateTime).ToList();
+
         }
 
         /// <summary>
@@ -19,15 +57,7 @@ namespace Antiban
         /// <returns></returns>
         public List<AntibanResult> GetResult()
         {
-            //TODO
-            //Example
-            var result = new List<AntibanResult>();
-            for (int i = 0; i < 10; i++)
-            {
-                result.Add(new AntibanResult());
-            }
-            
-            return result;
+            return results;
         }
     }
 }
--- Antiban/AntibanResult.cs
+++ Antiban/AntibanResult.cs
@@ -9,5 +9,7 @@ namespace Antiban
         /// </summary>
         public DateTime SentDateTime { get; set; }
         public int EventMessageId { get; set; }
+        public string Phone { get; set; }
+        public int Priority { get; set; }
     }
 }
